5babea2c093384ff1c8a1dc3c38bb2db34d2063e
Thibaud Merlin <thibaud.merlin@kyos.ch>
2023-01-23 11:13:12 +0100

    update

---
+++ Intune-Scripts/Proactive-remediation/DefenderATPRunning/detection.ps1
@@ -0,0 +1,71 @@
+<#
+
+.SYNOPSIS
+    PowerShell script to detect if Defender ATP Service is installed and running.
+
+.EXAMPLE
+    .\UAT-Win10-01-Detect-DefenderATPRunning.ps1
+
+.DESCRIPTION
+    This PowerShell script is deployed as a detection script using Proactive Remediations in Microsoft Endpoint Manager/Intune.
+
+.LINK
+    https://docs.microsoft.com/en-us/mem/analytics/proactive-remediations
+
+.NOTES
+    Version:        1.0
+    Creation Date:  January 23, 2023
+    Author:         Thibaud Merlin
+
+#>
+
+[CmdletBinding()]
+
+Param (
+
+)
+
+# Set Variables
+$ServiceName = "SENSE"
+$RegKeyFullPaths = @("HKLM:\SOFTWARE\Microsoft\Windows Advanced Threat Protection\Status")
+$DefenderOnboardingKey = "OnboardingState"
+$OnboardingStateValue = 1
+$RegKeyDefenderValue = (get-itemproperty $RegKeyFullPaths -name $DefenderOnboardingKey -ErrorAction Ignore).OnboardingState
+
+Try {
+    $Service = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
+    If ($null -eq $Service) {
+        Write-host 'Defender ATP is not installed. Remediation required.'
+        Write-Verbose 'Defender ATP is not installed. Remediation required.'
+        Exit 1 
+    }
+    Else {
+        If ($Service.Status -ne "Running") {
+            Write-host 'Defender ATP Service is not running. Remediation required.'
+            Write-Verbose 'Defender ATP Service is not running. Remediation required.'
+            Exit 1
+        }
+        Else {
+            If ($RegKeyDefenderValue -eq $OnboardingStateValue) {
+                write-host 'Defender ATP is installed and running. No remediation required.'
+                Write-Verbose 'Defender ATP is installed and running. No remediation required.'
+                Exit 0
+            }
+            Else {
+                Write-host 'Defender ATP Service is running but not well onboarded. Remediation required.'
+                Write-Verbose 'Defender ATP Service is running but not well onboarded. Remediation required.'
+                Exit 1
+            }
+            
+
+        }
+    }   
+}
+
+Catch {
+
+    $ErrorMessage = $_.Exception.Message 
+    Write-Warning $ErrorMessage
+    Exit 1
+
+}
---
+++ Intune-Scripts/Proactive-remediation/DefenderATPRunning/remediation.ps1
@@ -0,0 +1,95 @@
+<#
+
+.SYNOPSIS
+    PowerShell script to remediate Defender ATP Service.
+
+.EXAMPLE
+    .\UAT-Win10-01-Remediate-DefenderATPRunning.ps1
+
+.DESCRIPTION
+    This PowerShell script is deployed as a remediation script using Proactive Remediations in Microsoft Endpoint Manager/Intune.
+
+.LINK
+    https://docs.microsoft.com/en-us/mem/analytics/proactive-remediations
+
+.NOTES
+    Version:        1.0
+    Creation Date:  January 23, 2023
+    Author:         Thibaud Merlin
+
+#>
+
+[CmdletBinding()]
+
+Param (
+
+)
+
+# Set Variables
+$sdbin = "0100048044000000540000000000000014000000020030000200000000001400FF0F120001010000000000051200000000001400E104120001010000000000050B0000000102000000000005200000002002000001020000000000052000000020020000"
+$WMIRegPath = @("HKLM:\SYSTEm\CurrentControlSet\Control\WMI\Security")
+$DataCollectionPath = @("HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection")
+$WATPPoliciesPath = @("HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection")
+$WMIKey1 = "14f8138e-3b61-580b-544b-2609378ae460"
+$WMIKey2 = "cb2ff72d-d4e4-585d-33f9-f3a395c40be7"
+$DataCollectionKey1 = "DisableEnterpriseAuthProxy"
+$DataCollectionValue1 = 1
+$WATPKey1 = "696C1FA1-4030-4FA4-8713-FAF9B2EA7C0A"
+$WATPKey2 = "OnboardingInfo"
+$WATPValue2 = "{"body":"{\"previousOrgIds\":[],\"orgId\":\"31cf5678-ba72-40a6-a007-9baff06b440a\",\"geoLocationUrl\":\"https://winatp-gw-neu.microsoft.com/\",\"datacenter\":\"NorthEurope\",\"vortexGeoLocation\":\"EU\",\"version\":\"1.4\"}","sig":"+QR0C9TZboRs4vHzA6zg3QST68HlYiDZHn0gA8lncS2iXrE6IySMqGITUhA8vUO6NBqUuaAUY4eHqJvs0akVCeVFGw8eJV81IFJCyzYVOccDLYZQL47MIrfE7i2aIVxeCdyJrfBFP1RtoOqu2vOrcFbigEYKPbNjrpZl9pgaXd9nmsZY15KDUTwjeN39/kb13mWf2/MYGAFLb00s6BQ/+nfBgcPkd2CcvMJ27PPAQuvCV5RtlJDhN5l/A0rftwBY3FmGiEiq55cKMhbHu/vuNRxCk13AI0gABQaLbO1rsxVZOupiyd6dK0QBfkSqm+h9gnd9E4YYCad3TK2olRJKmA==","sha256sig":"yZpLMEK6cSc2fK5PjuV0sW1XeGBe/RyWiSOyc/tn9jp8PlMsKhq7t5P5JdsUlATmh2ez+8v526t+OsMndugwwVPgTRUvyXIIIn+wXzCgdJe0IHFH0u08BRmlvv2ZZ4uKOzWo91NNne3XdAX8+ukInCS9UuzUiTQPOc+6L+uxfNQ44OjTdta/bsJEETMKqxTNTpqGB8Yzt/h1JNogiK8BCKHqmt0TZQkwnuIpG+h3vCCK8S6gJJkPWe+0qlWPemiDzP4m5le/K0E/6ClWDh14JKBfLoHeC42nbcMjggOWthRvKMp1aSrc3tu3xRPwn/OR8mCFzwU1hGSeBTJuS30pYw==","cert":"MIIFgzCCA2ugAwIBAgITMwAAAbnvaa3BtdDiyQAAAAABuTANBgkqhkiG9w0BAQsFADB+MQswCQYDVQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQMA4GA1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9uMSgwJgYDVQQDEx9NaWNyb3NvZnQgU2VjdXJlIFNlcnZlciBDQSAyMDExMB4XDTIxMDcwMTE5MTQ0OFoXDTIyMDcwMTE5MTQ0OFowHjEcMBoGA1UEAxMTU2V2aWxsZS5XaW5kb3dzLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANwQqmQnh8zPAWsIqT9w8fO/isnLjIq7xGqSBGJd85GZRC2PS/hHJEtxLhKblzBiPwu9MAEkDx6yp+uDpf1hMkIYDo47D/R67fvAcQ0TJ82TdBs8byYBsIsyulf16Tw6QMyZssaDd7W9wFc1pTmB60B6ybx9BVcGxHe5HMzNfmWpcC/+jl9DZpJTAJPjPGmw4JBe2uTkx/M3kfohWjD6vTzLCDtFGU+YvK9n/Tky8AYy7iOflff4HsqrQfsjvLPB4Eqf5DH6dd+OpfScpmpWq23GTVwYMLIVtkgG3pzWS6Gt1f7wxFjpV0qFKix/ROQ+QqcsXisymMdLEP07mhYpeVECAwEAAaOCAVgwggFUMA4GA1UdDwEB/wQEAwIFIDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwDAYDVR0TAQH/BAIwADAeBgNVHREEFzAVghNTZXZpbGxlLldpbmRvd3MuY29tMB0GA1UdDgQWBBRiSr3YSZ29UH7giX6oEKqOUnf85jAfBgNVHSMEGDAWgBQ2VollSctbmy88rEIWUE2RuTPXkTBTBgNVHR8ETDBKMEigRqBEhkJodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpb3BzL2NybC9NaWNTZWNTZXJDQTIwMTFfMjAxMS0xMC0xOC5jcmwwYAYIKwYBBQUHAQEEVDBSMFAGCCsGAQUFBzAChkRodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpb3BzL2NlcnRzL01pY1NlY1NlckNBMjAxMV8yMDExLTEwLTE4LmNydDANBgkqhkiG9w0BAQsFAAOCAgEANseRAsrd/3LBKGAR9PO4QG9qXMrYcsPMmAruZGWe2hLBVdj5Vq5RaDs+PUisS08Jf5kkQBLRiwx061a4U9YrobNVdP/FUjwq8UJSHxWVr3erVSazOqCY+ZOYRQgBJBtzi4nhKV/L0+G8uxj/r2yiHBuQeWHI/eeXOd+/bw/3BkdUTgENrrtm4fXanuHyaSHj/q+g4ea/cqrOuD+iIb+gaKM/5e8pWJ0McF3dYwUvBcH0FfxKjegKrsCBU+Y+BmEir8NEHXN7ZUVGx1BiW5DOBjgjCqYo5uxE4bztMmijb5cuH3GbQXPmfGm7GKBN+S7zyA+qK4xanS4cCqaVvZpIYXoPy4CTGXyctyAFLDTybkcxuXU2UqD+k43UkrTpgvZfzAu0XeWkcmNfHsuJOp+YA3Bxq1DUAtdvNwE+oQ0LQhjvqhzE9+nTykXFQq5mVZlXYM3G/Y3lGyxDMqfyEAFnT+nYLbRhnkN6Nidhfe9MKRNSu2jKzfkmYoIGIaWW/bd7WnCDd75DhIgsCW9LHAikaT2jb+JiP9R1grsY3kf98g9KO2gIQKNyifiVYrZQn02wXVfrEh2Qelvom4lBERrU3B/W5mmph4UF3X3iU5lCv55OcoHU2FY4EusnQoxAmBMRz6yxxHZqVuc8IW3G8jxuNu0HaB9vZ+iMEkd9sEIfMpA=","chain":["MIIG2DCCBMCgAwIBAgIKYT+3GAAAAAAABDANBgkqhkiG9w0BAQsFADCBiDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hpbmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEyMDAGA1UEAxMpTWljcm9zb2Z0IFJvb3QgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IDIwMTEwHhcNMTExMDE4MjI1NTE5WhcNMjYxMDE4MjMwNTE5WjB+MQswCQYDVQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQMA4GA1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9uMSgwJgYDVQQDEx9NaWNyb3NvZnQgU2VjdXJlIFNlcnZlciBDQSAyMDExMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA0AvApKgZgeI25eKq5fOyFVh1vrTlSfHghPm7DWTvhcGBVbjz5/FtQFU9zotq0YST9XV8W6TUdBDKMvMj067uz54EWMLZR8vRfABBSHEbAWcXGK/G/nMDfuTvQ5zvAXEqH4EmQ3eYVFdznVUr8J6OfQYOrBtU8yb3+CMIIoueBh03OP1y0srlY8GaWn2ybbNSqW7prrX8izb5nvr2HFgbl1alEeW3Utu76fBUv7T/LGy4XSbOoArX35Ptf92s8SxzGtkZN1W63SJ4jqHUmwn4ByIxcbCUruCw5yZEV5CBlxXOYexl4kvxhVIWMvi1eKp+zU3sgyGkqJu+mmoE4KMczVYYbP1rL0I+4jfycqvQeHNye97sAFjlITCjCDqZ75/D93oWlmW1w4Gv9DlwSa/2qfZqADj5tAgZ4Bo1pVZ2Il9q8mmuPq1YRk24VPaJQUQecrG8EidT0sH/ss1QmB619Lu2woI52awb8jsnhGqwxiYL1zoQ57PbfNNWrFNMC/o7MTd02Fkr+QB5GQZ7/RwdQtRBDS8FDtVrSSP/z834eoLP2jwt3+jYEgQYuh6Id7iYHxAHu8gFfgsJv2vd405bsPnHhKY7ykyfW2Ip98eiqJWIcCzlwT88UiNPQJrDMYWDL78p8R1QjyGWB87v8oDCRH2bYu8vw3eJq0VNUz4CedMCAwEAAaOCAUswggFHMBAGCSsGAQQBgjcVAQQDAgEAMB0GA1UdDgQWBBQ2VollSctbmy88rEIWUE2RuTPXkTAZBgkrBgEEAYI3FAIEDB4KAFMAdQBiAEMAQTALBgNVHQ8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAfBgNVHSMEGDAWgBRyLToCMZBDuRQFTuHqp8cx0SOJNDBaBgNVHR8EUzBRME+gTaBLhklodHRwOi8vY3JsLm1pY3Jvc29mdC5jb20vcGtpL2NybC9wcm9kdWN0cy9NaWNSb29DZXJBdXQyMDExXzIwMTFfMDNfMjIuY3JsMF4GCCsGAQUFBwEBBFIwUDBOBggrBgEFBQcwAoZCaHR0cDovL3d3dy5taWNyb3NvZnQuY29tL3BraS9jZXJ0cy9NaWNSb29DZXJBdXQyMDExXzIwMTFfMDNfMjIuY3J0MA0GCSqGSIb3DQEBCwUAA4ICAQBByGHB9VuePpEx8bDGvwkBtJ22kHTXCdumLg2fyOd2NEavB2CJTIGzPNX0EjV1wnOl9U2EjMukXa+/kvYXCFdClXJlBXZ5re7RurguVKNRB6xo6yEM4yWBws0q8sP/z8K9SRiax/CExfkUvGuV5Zbvs0LSU9VKoBLErhJ2UwlWDp3306ZJiFDyiiyXIKK+TnjvBWW3S6EWiN4xxwhCJHyke56dvGAAXmKX45P8p/5beyXf5FN/S77mPvDbAXlCHG6FbH22RDD7pTeSk7Kl7iCtP1PVyfQoa1fB+B1qt1YqtieBHKYtn+f00DGDl6gqtqy+G0H15IlfVvvaWtNefVWUEH5TV/RKPUAqyL1nn4ThEO792msVgkn8Rh3/RQZ0nEIU7cU507PNC4MnkENRkvJEgq5umhUXshn6x0VsmAF7vzepsIikkrw4OOAd5HyXmBouX+84Zbc1L71/TyH6xIzSbwb5STXq3yAPJarqYKssH0uJ/Lf6XFSQSz6iKE9s5FJlwf2QHIWCiG7pplXdISh5RbAU5QrM5l/Eu9thNGmfrCY498EpQQgVLkyg9/kMPt5fqwgJLYOsrDSDYvTJSUKJJbVuskfFszmgsSAbLLGOBG+lMEkc0EbpQFv0rW6624JKhxJKgAlN2992uQVbG+C7IHBfACXH0w76Fq17Ip5xCA==","MIIF7TCCA9WgAwIBAgIQP4vItfyfspZDtWnWbELhRDANBgkqhkiG9w0BAQsFADCBiDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hpbmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEyMDAGA1UEAxMpTWljcm9zb2Z0IFJvb3QgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IDIwMTEwHhcNMTEwMzIyMjIwNTI4WhcNMzYwMzIyMjIxMzA0WjCBiDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hpbmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEyMDAGA1UEAxMpTWljcm9zb2Z0IFJvb3QgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IDIwMTEwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoICAQCygEGqNThNE3IyaCJNuLLx/9VSvGzH9dJKjDbu0cJcfoyKrq8TKG/Ac+M6ztAlqFo6be+ouFmrEyNozQwph9FvgFyPRH9dkAFSWKxRxV8qh9zc2AodwQO5e7BW6KPeZGHCnvjzfLnsDbVU/ky2ZU+I8JxImQxCCwl8MVkXeQZ4KI2JOkwDJb5xalwL54RgpJki49KvhKSn+9GY7Qyp3pSJ4Q6g3MDOmT3qCFK7VnnkH4S6Hri0xElcTzFLh93dBWcmmYDgcRGjuKVB4qRTufcyKYMME782XgSzS0NHL2vikR7TmE/dQgfI6B0S/Jmpaz6SfsjWaTr8ZL22CZ3K/QwLopt3YEsDlKQwaRLWQi3BQUzK3Kr9j1uDRprZ/LHR47PJf0h6zSTwQY9cdNCssBAgBkm3xy0hyFfj0IbzA2j70M5xwYmZSmQBbP3sMJHPQTySx+W6hh1hhMdfgzlirrSSL0fzC/hV66AfWdC7dJse0Hbm8ukG1xDo+mTeacY1logC8Ea4PyeZb8txiSk190gWAjWP1Xl8TQLPX+uKg09FcYj5qQ1OcunCnAfPSRtOBA5jUYxe2ADBVSy2xuDCZU7JNDn1nLPEfuhhbhNfFcRf2X7tHc7uROzLLoax7Dj2cO2rXBPB2Q8Nx4CyVe0096yb5MPa50c8prWPMd/FS6/r8QIDAQABo1EwTzALBgNVHQ8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUci06AjGQQ7kUBU7h6qfHMdEjiTQwEAYJKwYBBAGCNxUBBAMCAQAwDQYJKoZIhvcNAQELBQADggIBAH9yzw+3xRXbm8BJyiZb/p4T5tPw0tuXX/JLP02zrhmu7deXoKzvqTqjwkGw5biRnhOBJAPmCf0/V0A5ISRW0RAvS0CpNoZLtFNXmvvxfomPEf4YbFGq6O0JlbXlccmh6Yd1phV/yX43VF50k8XDZ8wNT2uoFwxtCJJ+i92Bqi1wIcM9BhS7vyRep4TXPw8hIr1LAAbblxzYXtTFC1yHblCk6MM4pPvLLMWSZpuFXst6bJN8gClYW1e1QGm6CHmmZGIVnYeWRbVmIyADixxzoNOieTPgUFmG2y/lAiXqcyqfABTINseSO+lOAOzYVgm5M0kS0lQLAausR7aRKX1MtHWAUgHoyoL2n8ysnI8X6i8msKtyrAv+nlEex0NVZ09Rs1fWtuzuUrc66U7h14GIvE+OdbtLqPA1qibUZ2dJsnBMO5PcHd94kIZysjik0dySTclY6ysSXNQ7roxrsIPlAT/4CTL2kzU0Iq/dNw13CYArzUgA8YyZGUcFAenRv9FO0OYoQzeZpApKCNmacXPSqs0xE2N2oTdvkjgefRI8ZjLny23h/FKJ3crWZgWalmG+oijHHKOnNlA8OqTfSm7mhzvO6/DggTedEzxSjr25HTTGHdUKaj2YKXCMiSrRq4IQSB/c9O+lxbtVGjhjhE63bK2VVOxlIhBJF7jAHscPrFRH"]}"
+$WATPServiceName = "SENSE"
+$WATPStatusPath = @("HKLM:\SOFTWARE\Microsoft\Windows Advanced Threat Protection\Status")
+$WATPStatusKey = "OnboardingState"
+$WATPStatusValue = 1
+$Source = @"
+using System; 
+using System.IO; 
+using System.Runtime.InteropServices; 
+using Microsoft.Win32.SafeHandles; 
+using System.ComponentModel; 
+public static class Elam{ [DllImport("Kernel32", CharSet=CharSet.Auto, SetLastError=true)] 
+public static extern bool InstallELAMCertificateInfo(SafeFileHandle handle); 
+public static void InstallWdBoot(string path) { 
+var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read); 
+var handle = stream.SafeFileHandle; 
+if (!InstallELAMCertificateInfo(handle)) { 
+throw new Win32Exception(Marshal.GetLastWin32Error()); 
+}
+} }
+"@
+#endregion
+
+#region script
+$WMIKeyValue1 = get-itemproperty $WMIRegPath -name $WMIKey1 -ErrorAction Ignore
+If ($WMIKeyValue1) {
+    
+}
+Else {
+    New-ItemProperty -Path $WMIRegPath -Name $WMIKey1 -PropertyType Binary -Value $sdbin -Force
+}
+$WMIKeyValue2 = get-itemproperty $WMIRegPath -name $WMIKey2 -ErrorAction Ignore
+If ($WMIKeyValue1) {
+    
+}
+Else {
+    New-ItemProperty -Path $WMIRegPath -Name $WMIKey2 -PropertyType Binary -Value $sdbin -Force
+}
+
+$DataCollectionKeyValue = (get-itemproperty $DataCollectionPath -name $DataCollectionKey1 -ErrorAction Ignore).DisableEnterpriseAuthProxy
+If ($DataCollectionKeyValue -eq $DataCollectionValue1) {
+    
+}
+Else {
+    Set-ItemProperty -Path $DataCollectionPath -Name $DataCollectionKey1 -PropertyType DWord -Value $DataCollectionValue1 -Force
+}
+
+
+Add-Type -TypeDefinition $Source
+$driverPath = $env:SystemRoot + '\System32\Drivers\WdBoot.sys'; 
+[Elam]::InstallWdBoot($driverPath)
+
+New-ItemProperty -Path $WATPPoliciesPath -Name $WATPKey2 -PropertyType Binary -Value $WATPValue2 -Force
+
+Start-Service -Name $WATPServiceName
+
+$RegKeyDefenderValue = (get-itemproperty $RegKeyFullPaths -name $DefenderOnboardingKey -ErrorAction Ignore).OnboardingState
